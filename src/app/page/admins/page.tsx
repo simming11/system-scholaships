ca4050ca6ed37d4e09f65f0e4b079a6901cd0f23
simming11 <abdulazem.m13@gmail.com>
2024-08-06 05:19:12 +0700

    fix

--- src/app/page/admins/page.tsx
+++ src/app/page/admins/page.tsx
@@ -1,13 +1,17 @@
+// src/app/page/admins/page.tsx
+
 "use client";
+
+import ScholarshipDetail from '../scholarships/[id]/page';
 import AdminComponent from './admins';
+
 const AdminPage = () => {
   return (
     <div>
-      <AdminComponent/>
+      <AdminComponent />
+      <ScholarshipDetail />
     </div>
-    
   );
 }
 
 export default AdminPage;
- 
