5cd7de2ebd1c8a3f2bfcca5f8145d1cc4a8ece76
simming11 <abdulazem.m13@gmail.com>
2024-08-06 05:03:49 +0700

    fix

--- src/app/page/admins/page.tsx
+++ src/app/page/admins/page.tsx
@@ -1,7 +1,4 @@
-// pages/admin.tsx
-"use client";
-import ScholarshipDetail from '../scholarships/[id]/page';
-import CreateScholarshipPage from '../scholarships/create/internal/CreateInternal';
+
 import AdminComponent from './admins';
 const AdminPage = () => {
   return (
